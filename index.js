84a58acd284c4b52b813a68102695989b57be9d6
Анна <anna.kuvakina91@gmail.com>
2024-08-01 15:28:28 +0300

    index go

---
+++ index.js
@@ -0,0 +1,9 @@
+import { initMongoConnection } from './src/db/initMongoConnection.js';
+import {setupServer} from "./src/server.js";
+
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+
+bootstrap();
