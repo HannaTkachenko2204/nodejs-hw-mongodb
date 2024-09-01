4d877989d2e8d635d21c41c522dc6aebae0bbf9e
Анна <anna.kuvakina91@gmail.com>
2024-09-01 10:15:03 +0300

    reset_password_of_email

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer';
-
 import { SMTP } from '../constants/index.js';
 import { env } from '../utils/env.js';
+import createHttpError from 'http-errors';
 
 const transporter = nodemailer.createTransport({
   host: env(SMTP.SMTP_HOST),
@@ -13,5 +13,13 @@ const transporter = nodemailer.createTransport({
 });
 
 export const sendEmail = async (options) => {
-  return await transporter.sendMail(options);
+  try {
+    return await transporter.sendMail(options);
+  } catch (error) {
+    console.error(error);
+    throw createHttpError(
+      500,
+      'Failed to send the email, please try again later.',
+    );
+  }
 };
